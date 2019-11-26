8966589db32b52eecaa8afc14602824bb44e4655
Kevin at MMS <x8neter@gmail.com>
2019-11-26 17:17:31 +1300

    Office 26Nov 17:17

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -1,8 +1,15 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, memo } from "react";
 import { getStoryIds, getStory } from "../services/hnApi";
 import { Story } from "../components/Story";
+import {
+  GlobalStyle,
+  StoryContainerWrapper
+} from "../styles/StoriesContainerStyles";
+import { useInfiniteScroll } from "../hooks/useInfiniteScroll";
 
 export const StoriesContainer = () => {
+  const { count } = useInfiniteScroll();
+
   const [storyIds, setStoryIds] = useState([]);
 
   useEffect(() => {
@@ -11,10 +18,13 @@ export const StoriesContainer = () => {
 
   return (
     <>
-      <h1>Hacker News Stories</h1>
-      {storyIds.map(storyId => (
-        <Story key={storyId} storyId={storyId} />
-      ))}
+      <GlobalStyle />
+      <StoryContainerWrapper data-test-id="stories-container">
+        <h1>Hacker News Stories</h1>
+        {storyIds.slice(0, count).map(storyId => (
+          <Story key={storyId} storyId={storyId} />
+        ))}
+      </StoryContainerWrapper>
     </>
   );
 };
